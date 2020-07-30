03782940fefadde45ee883128be0f494abd40698
Thomas Claireau <thomas.claireau@gmail.com>
2020-07-30 18:27:04 +0200

    add global styles to app

--- components/GlobalStyles/GlobalStyles.js
+++ components/GlobalStyles/GlobalStyles.js
@@ -1,5 +1,23 @@
 import { Global, css } from '@emotion/core';
 
+const theme = {
+	fonts: {
+		mainFont: 'Open Sans',
+	},
+	colors: {
+		primary: '#34485e',
+		secondary: '#ecf0f0',
+	},
+	breakpoints: {
+		'break-mini': '375px',
+		'break-small': '425px',
+		'break-tablet': '768px',
+		'break-medium': '1024px',
+		'break-large': '1210px',
+		'break-header': '1400px',
+	},
+};
+
 const GlobalStyles = () => (
 	<>
 		<Global
@@ -7,10 +25,17 @@ const GlobalStyles = () => (
 				body {
 					margin: 0;
 					padding: 0;
+					font-family: ${theme.fonts.mainFont};
+				}
+
+				*,
+				*:before,
+				*:after {
+					box-sizing: border-box;
 				}
 			`}
 		/>
 	</>
 );
 
-export default GlobalStyles;
+export { theme, GlobalStyles };
--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,6 @@
 import { ThemeProvider } from 'emotion-theming';
-import GlobalStyles from 'components/GlobalStyles/GlobalStyles';
+import { theme, GlobalStyles } from 'components/GlobalStyles/GlobalStyles';
 import Header from 'components/Header';
-
-const theme = {
-	fonts: {
-		mainFont: 'OpenSans',
-	},
-	colors: {
-		primary: '#34485e',
-		secondary: '#ecf0f0',
-	},
-	breakpoints: {
-		'break-mini': '375px',
-		'break-small': '425px',
-		'break-tablet': '768px',
-		'break-medium': '1024px',
-		'break-large': '1210px',
-		'break-header': '1400px',
-	},
-};
 
 function MyApp({ Component, pageProps }) {
 	return (
