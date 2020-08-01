e499d8dff325aa12aa4f6621f11314209218f0a8
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-01 13:54:42 +0200

    add alias node_modules

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
 	webpack: (config) => {
 		config.resolve.alias['components'] = path.join(__dirname, 'components');
 		config.resolve.alias['public'] = path.join(__dirname, 'public');
+		config.resolve.alias['node_modules'] = path.join(__dirname, 'node_modules');
 
 		return config;
 	},
