1c053c681677ac8932adeb13285c12a97332efaf
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-01 13:54:06 +0200

    add fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import { theme, GlobalStyles } from 'components/GlobalStyles/GlobalStyles';
 import Header from 'components/Header';
 import Box from 'components/Box';
 
+//lib
+import 'node_modules/font-awesome/css/font-awesome.min.css';
+
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
