6de6877f437973cfaa9983033b6e2a48d5d997f8
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-01 22:42:20 +0200

    fix padding colonne

--- components/Col.jsx
+++ components/Col.jsx
@@ -1,20 +1,32 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/core';
 import { useRouter } from 'next/router';
 
 export default function Col({ direction, bg, children }) {
 	const router = useRouter();
+	const isHome = router.pathname == '/';
+	let padding;
 
-	console.log(router);
+	if (isHome) {
+		padding = css`
+			${`padding-${direction}`}: 60px;
+		`;
+	} else {
+		padding = css`
+			padding-right: 60px;
+			padding-left: 60px;
+		`;
+	}
+
 	const ColStyled = styled.div`
 		width: 100%;
 		height: 100%;
 		display: flex;
-		justify-content: ${router.pathname !== '/'
-			? 'center'
-			: direction == 'left'
-			? 'flex-end'
-			: 'flex-start'};
+		justify-content: ${!isHome ? 'center' : direction == 'left' ? 'flex-end' : 'flex-start'};
 		align-items: center;
+		padding-top: 60px;
+		padding-bottom: 60px;
+		${padding}
 	`;
 
 	return <ColStyled className={`${direction} --bg-${bg}`}>{children}</ColStyled>;
