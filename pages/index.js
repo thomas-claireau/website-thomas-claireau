d55f88f745fe605d95d76226a18e4b418ee7fb7c
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-01 13:54:30 +0200

    fix error not title in _document.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
-export default function Home() {
-	return <div></div>;
+import Head from 'next/head';
+
+function Home() {
+	return (
+		<>
+			<Head>
+				<title>Thomas Claireau | Fullstack Web Developer</title>
+			</Head>
+		</>
+	);
 }
+
+export default Home;
