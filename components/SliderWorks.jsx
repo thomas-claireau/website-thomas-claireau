f1c5da7d610dc4acabe2db11b924ef2b0a3c41c2
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-07 06:47:19 +0200

    remove swiper (text fix error)

--- components/SliderWorks.jsx
+++ components/SliderWorks.jsx
@@ -1,21 +1,3 @@
-import Swiper from 'swiper';
-import { useRef, useEffect } from 'react';
-
 export default function SliderWorks({ children }) {
-	const swiper = useRef(null);
-
-	useEffect(() => {
-		swiper.current = new Swiper('.swiper-container', {});
-	}, []);
-
-	return (
-		<div className="swiper-container">
-			<div className="swiper-wrapper">
-				<div className="swiper-slide">slide 1</div>
-				<div className="swiper-slide">slide 2</div>
-				<div className="swiper-slide">slide 3</div>
-				<div className="swiper-slide">slide 4</div>
-			</div>
-		</div>
-	);
+	return <div>coucou</div>;
 }
