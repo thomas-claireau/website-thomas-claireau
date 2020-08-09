1b9c08ba3ec13660548a643ea5ab32de2c28c5c9
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-09 10:56:05 +0200

    remove get server side props

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,19 @@ import ContentVM from 'components/ContentVM';
 import SliderWorks from 'components/SliderWorks';
 import fetch from 'isomorphic-unfetch';
 
-function Home({ fields }) {
+function Home() {
+	const fields = {
+		titre_haut_gauche: 'test',
+		titre_haut_droite: 'test',
+		titre_bas_gauche: 'test',
+		titre_bas_droite: 'test',
+		header: {
+			meta_description: 'test',
+			meta_title: 'test',
+			title: 'test',
+		},
+	};
+
 	const {
 		titre_haut_gauche,
 		titre_haut_droite,
@@ -88,19 +100,6 @@ function Home({ fields }) {
 			</HomeStyled>
 		</>
 	);
-}
-
-export async function getServerSideProps() {
-	const { API_URL } = process.env;
-
-	const res = await fetch(`${API_URL}/accueil`);
-	const data = await res.json();
-
-	return {
-		props: {
-			fields: data,
-		},
-	};
 }
 
 export default Home;
