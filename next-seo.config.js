fb686f00f12d1084670f5a80450d306c54e929b4
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-09 09:10:05 +0200

    update next seo config

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,12 +1,12 @@
 export default {
-	title: 'Thomas Claireau | Fullstack web developer',
-	description: "I'm a fullstack web developer",
+	title: 'Thomas Claireau | Frontend web developer',
+	description: "I'm a frontend web developer",
 	canonical: 'https://thomas-claireau.fr/',
 	openGraph: {
 		type: 'website',
 		locale: 'en_EN',
 		url: 'https://www.thomas-claireau.fr',
-		title: 'Thomas Claireau | Fullstack web developer',
+		title: 'Thomas Claireau | Frontend web developer',
 		site_name: 'Thomas Claireau',
 	},
 	twitter: {
