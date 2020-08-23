687cf3cc7bdc9d7da56fd779c855bc845d92ebf7
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-23 11:31:43 +0200

    travis ci -> test sur recette

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 set -xe
 
-if [ $TRAVIS_BRANCH == 'master' ]; then
-	$ERRORSTRING="Error. Please make sure you've indicated correct parameters"
-	if [ $# -eq 0 ]; then
+# if [ $TRAVIS_BRANCH == 'recette' ]; then
+$ERRORSTRING="Error. Please make sure you've indicated correct parameters"
+if [ $# -eq 0 ]; then
+	echo $ERRORSTRING
+elif [ $1 == "live" ]; then
+	if [[ -z $2 ]]; then
+		echo "Running dry-run"
+		rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p $PORT" ./ $USERNAME@$HOST:$PROJECT
+	elif [ $2 == "go" ]; then
+		echo "Running actual deploy"
+		rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p $PORT" ./ $USERNAME@$HOST:$PROJECT
+	else
 		echo $ERRORSTRING
-	elif [ $1 == "live" ]; then
-		if [[ -z $2 ]]; then
-			echo "Running dry-run"
-			rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p $PORT" ./ $USERNAME@$HOST:$PROJECT
-		elif [ $2 == "go" ]; then
-			echo "Running actual deploy"
-			rsync -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p $PORT" ./ $USERNAME@$HOST:$PROJECT
-		else
-			echo $ERRORSTRING
-		fi
 	fi
-else
-	echo "Not deploying, since this branch isn't master."
 fi
+# else
+# 	echo "Not deploying, since this branch isn't recette."
+# fi
