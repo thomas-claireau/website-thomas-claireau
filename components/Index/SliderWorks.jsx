482996562653c820bd5b3aaaf31f97ceb76d4db7
Thomas Claireau <thomas.claireau@gmail.com>
2020-08-27 10:16:21 +0200

    fix title link slider work

--- components/Index/SliderWorks.jsx
+++ components/Index/SliderWorks.jsx
@@ -105,8 +105,8 @@ export default function SliderWorks({ data }) {
 
 					return (
 						<SwiperSlide key={id}>
-							<Link href={`${view_all_link}/${id}`} title={main_image.caption}>
-								<a>
+							<Link href={`${view_all_link}/${id}`}>
+								<a title={main_image.caption}>
 									<img src={main_image.url} alt={main_image.caption} />
 								</a>
 							</Link>
