f0fe644c539629d9673c16124ef88af6800a1596
Thomas Claireau <thomas.claireau@gmail.com>
2020-09-06 12:07:31 +0200

    refacto scss -> projets page ok

--- pages/projets.jsx
+++ pages/projets.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { NextSeo } from 'next-seo';
 import { useQuery } from '@apollo/react-hooks';
 import { withApollo } from 'libs/apollo';
 import PROJETS_QUERY from 'apollo/queries/projets';
+
+import styles, { projets } from 'styles/pages/projets.module.scss';
 
 import Container from 'components/Global/Layout/Container';
 import Menu from 'components/Global/Menus/Menu';
@@ -13,50 +14,6 @@ import { Loading } from 'components/Global/Loading';
 import { Error } from 'components/Global/Error';
 
 function Projets() {
-	const ProjetsStyled = styled.div`
-		position: relative;
-		width: 100%;
-		height: 100%;
-		padding-bottom: 100px;
-		overflow: hidden;
-
-		@media screen and (max-width: ${(props) => props.theme.breakpoints['break-large']}) {
-			padding-bottom: 0px;
-			overflow-x: hidden;
-		}
-
-		&:before {
-			content: '';
-			width: 50%;
-			height: 100%;
-			position: absolute;
-			top: 0;
-			right: 0;
-			background-color: ${(props) => props.theme.colors.light};
-
-			@media screen and (max-width: ${(props) => props.theme.breakpoints['break-large']}) {
-				width: 100%;
-				height: 50%;
-				top: auto;
-				right: auto;
-				bottom: 0;
-				left: 0;
-			}
-		}
-
-		.main-content {
-			width: 100%;
-			height: 100%;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-
-			@media screen and (max-width: ${(props) => props.theme.breakpoints['break-large']}) {
-				padding-bottom: 0px;
-			}
-		}
-	`;
-
 	const { data, loading, error } = useQuery(PROJETS_QUERY);
 
 	if (loading) return <Loading />;
@@ -75,14 +32,14 @@ function Projets() {
 	return (
 		<>
 			<NextSeo {...SEO} />
-			<ProjetsStyled>
-				<Container className="main-content">
+			<section className={projets}>
+				<Container className={styles['main-content']}>
 					<h1 className="--hide">Thomas Claireau - {data.meta.header.title}</h1>
 					<Slider projets={data.projets} />
 					<Menu space="60px" className="desktop" />
 				</Container>
 				<MenuBottom></MenuBottom>
-			</ProjetsStyled>
+			</section>
 		</>
 	);
 }
