90645ac4f052e3b91e061023ae819282de422c23
Thomas Claireau <thomas.claireau@gmail.com>
2020-09-06 11:18:52 +0200

    refacto scss -> home page ok

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,15 @@ import { useQuery } from '@apollo/react-hooks';
 import { withApollo } from 'libs/apollo';
 import INDEX_QUERY from 'apollo/queries/index';
 
-import styled from '@emotion/styled';
+import {
+	home,
+	title,
+	desktop,
+	mobile,
+	left,
+	right,
+	description,
+} from 'styles/pages/index.module.scss';
 
 import Container from 'components/Global/Layout/Container';
 import Col from 'components/Global/Layout/Col';
@@ -14,57 +22,6 @@ import { Loading } from 'components/Global/Loading';
 import { Error } from 'components/Global/Error';
 
 function Home() {
-	const HomeStyled = styled.div`
-		width: 100%;
-		height: 100%;
-		display: flex;
-		overflow: hidden;
-
-		@media screen and (max-width: ${(props) => props.theme.breakpoints['break-large']}) {
-			flex-direction: column;
-			overflow-y: visible;
-		}
-
-		> div > .title {
-			display: flex;
-			flex-direction: column;
-
-			&.desktop {
-				@media screen and (max-width: ${(props) =>
-						props.theme.breakpoints['break-large']}) {
-					display: none;
-				}
-			}
-
-			&.mobile {
-				display: none;
-
-				@media screen and (max-width: ${(props) =>
-						props.theme.breakpoints['break-large']}) {
-					display: inherit;
-				}
-			}
-
-			&.left {
-				align-items: flex-end;
-			}
-		}
-
-		.description {
-			margin-top: 50px;
-			font-size: 20px;
-			font-weight: 300;
-
-			&.left {
-				margin-right: 7px;
-			}
-
-			&.right {
-				margin-left: 5px;
-			}
-		}
-	`;
-
 	const { data, loading, error } = useQuery(INDEX_QUERY);
 
 	if (loading) return <Loading />;
@@ -85,20 +42,20 @@ function Home() {
 	return (
 		<>
 			<NextSeo {...SEO} />
-			<HomeStyled>
+			<section className={home}>
 				<Col direction="left" bg="--bg-dark" align="flex-end">
-					<div className="title h1 left --light desktop">
+					<div className={`${title} ${left} ${desktop} h1 --light`}>
 						<span>{accueil.titre_haut_gauche}</span>
 						<span>{accueil.titre_bas_gauche}</span>
 					</div>
-					<p className="description left --light">{accueil.description_gauche}</p>
+					<p className={`${description} ${left} --light`}>{accueil.description_gauche}</p>
 				</Col>
 				<Col direction="right" bg="--bg-light" align="flex-start">
-					<div className="title h1 --dark desktop">
+					<div className={`${title} ${desktop} h1 --dark`}>
 						<span>{accueil.titre_haut_droite}</span>
 						<span>{accueil.titre_bas_droite}</span>
 					</div>
-					<p className="description right --dark">{accueil.description_droite}</p>
+					<p className={`${description} ${right} --dark`}>{accueil.description_droite}</p>
 				</Col>
 				<ContentVM>
 					<Container>
@@ -107,7 +64,7 @@ function Home() {
 					</Container>
 					<SliderWorks data={accueil.slide_work}></SliderWorks>
 				</ContentVM>
-			</HomeStyled>
+			</section>
 		</>
 	);
 }
