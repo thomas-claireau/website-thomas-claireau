76f2562e0fb580f9f18fa4dd134e0df9fc174c7d
Thomas Claireau <thomas.claireau@gmail.com>
2020-09-15 06:22:31 +0200

    fix warning content html

--- components/global/HtmlContent/index.jsx
+++ components/global/HtmlContent/index.jsx
@@ -1,20 +1,26 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import marked from 'marked';
 import Prism from 'prismjs';
 import { content } from './index.module.scss';
 
 export default function HtmlContent({ children, className, ...props }) {
+	const [htmlContent, setHtmlContent] = useState(<div></div>);
+
 	useEffect(() => {
 		Prism.highlightAll();
+	}, [htmlContent]);
+
+	useLayoutEffect(() => {
+		setHtmlContent(
+			<div
+				{...props}
+				className={`${content} ${className}`}
+				dangerouslySetInnerHTML={{
+					__html: marked(children),
+				}}
+			/>
+		);
 	}, []);
 
-	return (
-		<div
-			{...props}
-			className={`${content} ${className}`}
-			dangerouslySetInnerHTML={{
-				__html: marked(children),
-			}}
-		></div>
-	);
+	return htmlContent;
 }
