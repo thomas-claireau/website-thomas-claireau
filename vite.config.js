26f0aa81995ce8171d7ebafa2b91fdda9f0ebd8e
Thomas Claireau <thomas.claireau@gmail.com>
2021-06-09 11:00:43 +0200

    full reload twig OK

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
+
+const twigRefreshPlugin = {
+  name: "twig-refresh",
+  configureServer({ watcher, ws }) {
+    watcher.add(resolve("templates/**/*.twig"));
+    watcher.on("change", function (path) {
+      if (path.endsWith(".twig")) {
+        ws.send({
+          type: "full-reload",
+        });
+      }
+    });
+  },
+};
 
 export default defineConfig({
+  plugins: [twigRefreshPlugin],
   root: "./assets",
   base: "/assets/",
+  server: {
+    watch: {
+      disableGlobbing: false,
+    },
+  },
   build: {
     manifest: true,
     assetsDir: "",
