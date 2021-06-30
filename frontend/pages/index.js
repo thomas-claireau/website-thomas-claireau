5bcdee9d5abc51274be33208df4ab0d0b34263cc
Thomas Claireau <thomas.claireau@gmail.com>
2021-06-30 09:37:33 +0200

    add frontend nextjs

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+	return (
+		<div className="container">
+			<Head>
+				<title>Thomas Claireau - Développeur PHP Symfony</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<style jsx>{``}</style>
+			<style jsx global>{``}</style>
+		</div>
+	);
+}
