f30d7644e22f8aead5c036887b01fb1ed9605ddb
Thomas Claireau <thomas.claireau@gmail.com>
2021-07-31 16:01:58 +0200

    fix Header container

--- frontend/components/Header/Header.jsx
+++ frontend/components/Header/Header.jsx
@@ -2,17 +2,20 @@ import { header, left } from './Header.module.scss';
 
 import Menu from '../Menu/Menu';
 import MenuMobile from '../MenuMobile/MenuMobile';
+import Container from '../Container/Container';
 
 export default function Header() {
 	return (
 		<header className={header}>
-			<div className={left}>
-				Thomas
-				<span>/</span>
-				Claireau
-			</div>
-			<Menu />
-			<MenuMobile />
+			<Container>
+				<div className={left}>
+					Thomas
+					<span>/</span>
+					Claireau
+				</div>
+				<Menu />
+				<MenuMobile />
+			</Container>
 		</header>
 	);
 }
