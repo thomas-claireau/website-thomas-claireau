1c2eb0d4fcad10c4378632e7a08d5c7d0c29ac39
Thomas Claireau <thomas.claireau@gmail.com>
2021-08-01 09:53:15 +0200

    add header window scrolled

--- frontend/components/Header/Header.jsx
+++ frontend/components/Header/Header.jsx
@@ -1,12 +1,23 @@
-import { header, left } from './Header.module.scss';
+import { header, left, scrolled } from './Header.module.scss';
 
 import Menu from '../Menu/Menu';
 import MenuMobile from '../MenuMobile/MenuMobile';
 import Container from '../Container/Container';
+import { useState, useEffect } from 'react';
 
 export default function Header() {
+	const [scroll, setScroll] = useState(0);
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+	}, []);
+
+	function handleScroll() {
+		setScroll(window.scrollY);
+	}
+
 	return (
-		<header className={header}>
+		<header className={`${header} ${scroll > 0 ? scrolled : ''}`}>
 			<Container>
 				<div className={left}>
 					Thomas
