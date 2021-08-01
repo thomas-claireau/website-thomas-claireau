62af81bf428d58594b84502dd5b98610b2ca58b9
Thomas Claireau <thomas.claireau@gmail.com>
2021-08-01 10:14:32 +0200

    clean repository

--- assets/js/global/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const footerDate = document.querySelector('footer .date');
-
-	if (footerDate) {
-		const now = new Date();
-		footerDate.innerHTML = now.getFullYear();
-	}
-});
--- assets/js/global/functions.js
+++
@@ -1,7 +0,0 @@
-import SVGInjector from "svg-injector";
-
-export function injectSvgs() {
-  const svgs = document.querySelectorAll("img.js-inject-me");
-
-  SVGInjector(svgs);
-}
