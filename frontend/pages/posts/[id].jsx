7c55de1c06fdcb706ae71fcde392f283514d2510
Thomas Claireau <thomas.claireau@gmail.com>
2021-08-06 19:06:38 +0200

    wip post -> add getStaticPath and props

---
+++ frontend/pages/posts/[id].jsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Container from '../../components/Container/Container';
+import Layout from '../../components/Layout/Layout';
+
+import style from './post.module.scss';
+
+export default function Post({ post }) {
+	return (
+		<Layout>
+			<Head>
+				<title>{post.title} - Thomas Claireau, développeur web</title>
+			</Head>
+			<Container className={style['post']}></Container>
+		</Layout>
+	);
+}
+
+export async function getStaticPaths() {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`);
+	const posts = await res.json();
+
+	const paths = posts.map((post) => {
+		return { params: { id: post.id.toString() } };
+	});
+
+	return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/posts/${params.id}`
+	);
+	const post = await res.json();
+
+	return { props: { post } };
+}
