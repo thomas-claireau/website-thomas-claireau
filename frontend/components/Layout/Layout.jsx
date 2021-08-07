a4c0748b16b3130e99a5ba388d9bf612f1e17341
Thomas Claireau <thomas.claireau@gmail.com>
2021-08-07 10:18:09 +0200

    Layout -> add footer at the bottom

--- frontend/components/Layout/Layout.jsx
+++ frontend/components/Layout/Layout.jsx
@@ -5,16 +5,18 @@ import Head from 'next/head';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
+import style from './Layout.module.scss';
+
 export default function Layout({ children }) {
 	return (
-		<>
+		<div className={style['layout']}>
 			<Head>
 				<title>Thomas Claireau - Développeur PHP Symfony</title>
 			</Head>
 			<Header />
 			{children}
 			<Footer />
-		</>
+		</div>
 	);
 }
 
