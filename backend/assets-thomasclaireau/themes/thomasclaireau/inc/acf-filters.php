65d99351dfe61577be455fdaa05b07ab6899e6a0
Thomas Claireau <thomas.claireau@gmail.com>
2021-08-20 15:36:33 +0200

    add acf filter for file

--- backend/assets-thomasclaireau/themes/thomasclaireau/inc/acf-filters.php
+++ backend/assets-thomasclaireau/themes/thomasclaireau/inc/acf-filters.php
@@ -6,22 +6,46 @@
  */
 
 add_filter( 'acf/format_value/type=image', 'acf_image_filter', 20, 3 );
+add_filter( 'acf/format_value/type=file', 'acf_file_filter', 20, 3 );
 
-/**
- * Acf Image filter
- *
- * @param mixed $value - Value of filter field.
- *
- * @return array
- */
-function acf_image_filter( $value ) {
-	if ( $value ) {
-		return array(
-			'url'   => $value['url'],
-			'alt'   => $value['alt'],
-			'sizes' => $value['sizes'],
-		);
+if ( ! function_exists( 'acf_image_filter' ) ) :
+	/**
+	 * Acf Image filter
+	 *
+	 * @param mixed $value - Value of filter field.
+	 *
+	 * @return array
+	 */
+	function acf_image_filter( $value ) {
+		if ( $value ) {
+			return array(
+				'url'   => $value['url'],
+				'alt'   => $value['alt'],
+				'sizes' => $value['sizes'],
+			);
+		}
+
+		return $value;
 	}
+endif;
 
-	return $value;
-}
+if ( ! function_exists( 'acf_file_filter' ) ) :
+	/**
+	 * Acf Image filter
+	 *
+	 * @param mixed $value - Value of filter field.
+	 *
+	 * @return array
+	 */
+	function acf_file_filter( $value ) {
+		if ( $value ) {
+			return array(
+				'url'       => $value['url'],
+				'alt'       => $value['alt'],
+				'mime_type' => $value['mime_type'],
+			);
+		}
+
+		return $value;
+	}
+endif;
