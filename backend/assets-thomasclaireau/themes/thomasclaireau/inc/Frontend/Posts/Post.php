9b3f1ee3f799e0148df01de4ad02581bfeb16834
Thomas Claireau <thomas.claireau@gmail.com>
2021-08-20 18:48:54 +0200

    Back | post -> retrieve content api

--- backend/assets-thomasclaireau/themes/thomasclaireau/inc/Frontend/Posts/Post.php
+++ backend/assets-thomasclaireau/themes/thomasclaireau/inc/Frontend/Posts/Post.php
@@ -20,6 +20,19 @@ class Post {
 	 */
 	public static function datas( $post_id ) {
 		$datas = array();
+		$post  = get_post( $post_id );
+
+		if ( is_null( $post ) ) {
+			return null;
+		}
+
+		$datas['title']            = $post->post_title;
+		$datas['thumbnail']['url'] = get_the_post_thumbnail_url( $post );
+
+		$thumbnail_id              = get_post_thumbnail_id( $post->ID );
+		$datas['thumbnail']['alt'] = get_post_meta( $thumbnail_id, '_wp_attachment_image_alt', true );
+
+		$datas['content'] = $post->post_content;
 
 		return $datas;
 	}
