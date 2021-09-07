a6c28b7b71b7ddca0558014a8ead46ba0442588b
Thomas Claireau <thomas.claireau@gmail.com>
2021-09-07 08:58:21 +0200

    remove old backend

--- old-backend/functions.js
+++
@@ -1,1 +0,0 @@
-exports.handleError = (res, error) => res.status(500).json({ error });
--- old-backend/middlewares/empty-body.js
+++
@@ -1,9 +0,0 @@
-// check if req.body is empty
-module.exports = (req, res, next) => {
-	const allowMethods = ['POST', 'PATCH'];
-
-	if (!Object.keys(req.body).length && allowMethods.includes(req.method))
-		return res.status(403).json({ error: 'The data body must not be empty' });
-
-	next();
-};
