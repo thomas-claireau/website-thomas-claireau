9c261c5a981092c38ea93ed0dbc809442b5ce3dc
Thomas Claireau <thomas.claireau@gmail.com>
2021-09-10 14:15:39 +0200

    init contact

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -2,9 +2,10 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import https from 'https';
 import 'moment/locale/fr';
-
 import Head from 'next/head';
+
 import { ThemeProvider } from '../components/ThemeProvider';
+import Contact from '../components/Contact/Contact';
 
 import '../styles/global.scss';
 import 'public/assets/css/fontawesome.min.css';
@@ -17,7 +18,6 @@ if (process.env.NODE_ENV === 'development') {
 	process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 }
 
-
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
 	const { global } = pageProps;
@@ -27,11 +27,12 @@ export default function MyApp({ Component, pageProps }) {
 			<Head>
 				<script
 					src="/assets/js/fontawesome.min.js"
-					referrerpolicy="no-referrer"
+					referrerPolicy="no-referrer"
 					defer
 				></script>
 			</Head>
 			<Component {...pageProps} />
+			<Contact />
 		</ThemeProvider>
 	);
 }
@@ -39,4 +40,5 @@ export default function MyApp({ Component, pageProps }) {
 MyApp.propTypes = {
 	Component: PropTypes.func.isRequired,
 	pageProps: PropTypes.object.isRequired,
+	fields: PropTypes.object,
 };
