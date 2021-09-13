d48f25bc2e05f526d245dc420b6dc2f606bc4e60
Thomas Claireau <thomas.claireau@gmail.com>
2021-09-13 15:36:03 +0200

    remove useless plugin

--- backend/assets-thomasclaireau/plugins/wp-seopress/inc/admin/page-builders/gutenberg/inc/blocks/faq-block/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '10d17760a800ed812a8b44fc9a21df48');
