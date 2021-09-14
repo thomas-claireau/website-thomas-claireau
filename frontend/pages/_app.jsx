844f9b8111ca186f047e30d90f12c0954b8c7986
Thomas Claireau <thomas.claireau@gmail.com>
2021-09-14 09:18:04 +0200

    axeptio -> fix script already loaded

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -22,47 +22,51 @@ export default function MyApp({ Component, pageProps }) {
 	const { global } = pageProps;
 
 	if (typeof window !== 'undefined') {
-		window.axeptioSettings = {
-			clientId: '608a63fa96c7dd3c31e28d9b',
-		};
+		if (!window.axeptioSettings) {
+			window.axeptioSettings = {
+				clientId: '608a63fa96c7dd3c31e28d9b',
+			};
 
-		(function (d, s) {
-			var t = d.getElementsByTagName(s)[0],
-				e = d.createElement(s);
-			e.async = true;
-			e.src = '//static.axept.io/sdk-slim.js';
-			t.parentNode.insertBefore(e, t);
-		})(document, 'script');
+			(function (d, s) {
+				var t = d.getElementsByTagName(s)[0],
+					e = d.createElement(s);
 
-		void 0 === window._axcb && (window._axcb = []);
-		window._axcb.push(function (axeptio) {
-			axeptio.on('cookies:complete', function (choices) {
-				if (choices.google_analytics) {
-					(function (i, s, o, g, r, a, m) {
-						i['GoogleAnalyticsObject'] = r;
-						(i[r] =
-							i[r] ||
-							function () {
-								(i[r].q = i[r].q || []).push(arguments);
-							}),
-							(i[r].l = 1 * new Date());
-						(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
-						a.async = 1;
-						a.src = g;
-						m.parentNode.insertBefore(a, m);
-					})(
-						window,
-						document,
-						'script',
-						'https://www.google-analytics.com/analytics.js',
-						'ga'
-					);
+				e.async = true;
+				e.src = '//static.axept.io/sdk-slim.js';
+				t.parentNode.insertBefore(e, t);
+			})(document, 'script');
 
-					ga('create', 'UA-119345981-2', 'auto');
-					ga('send', 'pageview');
-				}
+			void 0 === window._axcb && (window._axcb = []);
+			window._axcb.push(function (axeptio) {
+				axeptio.on('cookies:complete', function (choices) {
+					if (choices.google_analytics) {
+						(function (i, s, o, g, r, a, m) {
+							i['GoogleAnalyticsObject'] = r;
+							(i[r] =
+								i[r] ||
+								function () {
+									(i[r].q = i[r].q || []).push(arguments);
+								}),
+								(i[r].l = 1 * new Date());
+							(a = s.createElement(o)),
+								(m = s.getElementsByTagName(o)[0]);
+							a.async = 1;
+							a.src = g;
+							m.parentNode.insertBefore(a, m);
+						})(
+							window,
+							document,
+							'script',
+							'https://www.google-analytics.com/analytics.js',
+							'ga'
+						);
+
+						ga('create', 'UA-119345981-2', 'auto');
+						ga('send', 'pageview');
+					}
+				});
 			});
-		});
+		}
 	}
 
 	return (
