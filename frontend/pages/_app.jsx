d5e8f5823ccfe0475c356feee10a1ccf308af0e1
Thomas Claireau <thomas.claireau@gmail.com>
2021-09-14 10:56:30 +0200

    app -> fix node tls

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -16,8 +16,8 @@ if (process.env.NODE_ENV === 'development') {
 		rejectUnauthorized: false,
 	});
 
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 }
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 config.autoAddCss = false;
 
